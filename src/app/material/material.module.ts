5a71a758bf96782a11adb28365524e211c1ef373
Praveen Sharma <praveen.sharma@decimal.co.in>
2025-03-20 19:53:03 +0530

    ng update @angular/material@15

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 
 const matModules = [MatInputModule, MatButtonModule];
 
